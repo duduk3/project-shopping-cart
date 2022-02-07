1a14b76a430c6531f7da0bc9c47ccd63a7a9df93
EDUARDO FERREIRA <dupearljam@gmail.com>
2022-02-07 09:34:48 -0300

    req11 => insere testes em getSavedCartItems

--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -5,12 +5,12 @@ localStorageSimulator('setItem');
 
 describe('4 - Teste a função saveCartItems', () => {
   it('Testa se, ao executar saveCartItems com o argumento <ol><li>Item</li></ol>, o método localStorage.setItem é chamado', () => {
-    const test = '<ol><li>Item</li></ol>';
+    const test = '<ol><li>Item</li></ol> (1)';
     saveCartItems(test);
     expect(localStorage.setItem).toHaveBeenCalled();
   })
   it('Testa se, ao executar saveCartItems com o argumento <ol><li>Item</li></ol>, o método localStorage.setItem é chamado com dois parâmetros, sendo o primeiro "cartItems" e o segundo sendo o valor passado como argumento para saveCartItems', () => {
-    const test = '<ol><li>Item</li></ol>';
+    const test = '<ol><li>Item</li></ol> (2)';
     saveCartItems(test);
     expect(localStorage.setItem).toHaveBeenCalledWith('cartItems', test);
   })
